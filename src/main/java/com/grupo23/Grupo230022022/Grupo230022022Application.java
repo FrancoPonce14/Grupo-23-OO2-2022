99345934f19ccf65cc599f8edbb9fede5ea7c4df
FrancoPonce14 <francoponce20@hotmail.com>
2022-05-15 20:38:51 -0300

    maven y demas

---
+++ src/main/java/com/grupo23/Grupo230022022/Grupo230022022Application.java
@@ -0,0 +1,13 @@
+package com.grupo23.Grupo230022022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo230022022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo230022022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo23/Grupo230022022/Grupo230022022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo23.Grupo230022022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo230022022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
