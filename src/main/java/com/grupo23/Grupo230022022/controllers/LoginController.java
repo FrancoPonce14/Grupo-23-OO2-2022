1d9b49c5a8defbf183f4b7d3b2daf870fb1a80d8
FrancoPonce14 <francoponce20@hotmail.com>
2022-05-15 22:26:48 -0300

    security default + index in controller

--- src/main/java/com/grupo23/Grupo230022022/controllers/LoginController.java
+++ src/main/java/com/grupo23/Grupo230022022/controllers/LoginController.java
@@ -1,5 +1,31 @@
 package com.grupo23.Grupo230022022.controllers;
 
-public class LoginController {
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.grupo23.Grupo230022022.helpers.ViewRouteHelper;
+
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+	
+	
+	@GetMapping("/")
+	public ModelAndView index(@RequestParam(name="perfil",required=false) String perfil) {
+		ModelAndView mAV = new ModelAndView(ViewRouteHelper.INDEX);
+		org.springframework.security.core.Authentication auth = (Authentication) SecurityContextHolder.getContext().getAuthentication();
+		perfil = (auth.getAuthorities().toString());
+		perfil = perfil.replace("[", ""); 
+		perfil = perfil.replace("]", ""); // para poder usar el perfil le saco las [ ] que tiene al inicio y final ya que es una coleccion 
+		mAV.addObject("perfil", perfil);
+		return mAV;
+	}
+	
+	
 }
---
+++ src/main/java/com/grupo23/Grupo230022022/helpers/ViewRouteHelper.java
@@ -0,0 +1,9 @@
+package com.grupo23.Grupo230022022.helpers;
+
+public class ViewRouteHelper {
+
+	// INICIO/CIERRE SESIÓN + INDEXS ADMIN - AUDITOR
+	public final static String LOGIN = "/login";
+	public final static String LOGOUT = "/logout";
+	public final static String INDEX = "/index";
+}
