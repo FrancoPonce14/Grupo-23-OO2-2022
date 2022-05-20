c1009b5c00a4eec0ae5bfe868d33cd6508850cdc
valentopssian <valentintopssian@gmail.com>
2022-05-20 18:18:00 -0300

    prueba

---
+++ bin/grupo23-bd-oo2-2022.sql
@@ -0,0 +1,118 @@
+drop database if exists `grupo23-bd-oo2-2022`;
+create database if not exists `grupo23-bd-oo2-2022`;
+use `grupo23-bd-oo2-2022`;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: localhost    Database: grupo23-bd-oo2-2022
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `perfil`
+--
+
+DROP TABLE IF EXISTS `perfil`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `perfil` (
+  `id_perfil` int NOT NULL AUTO_INCREMENT,
+  `habilitado` bit(1) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_perfil`),
+  UNIQUE KEY `UK_3b0dloqo94v7r6tjahpid9hc3` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `perfil`
+--
+
+LOCK TABLES `perfil` WRITE;
+/*!40000 ALTER TABLE `perfil` DISABLE KEYS */;
+INSERT INTO `perfil` VALUES (1,_binary '','PERFIL_ADMIN'),(2,_binary '','PERFIL_AUDITOR');
+/*!40000 ALTER TABLE `perfil` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `persona`
+--
+
+DROP TABLE IF EXISTS `persona`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `persona` (
+  `id_persona` int NOT NULL AUTO_INCREMENT,
+  `apellido` varchar(255) NOT NULL,
+  `documento` bigint NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_persona`),
+  UNIQUE KEY `UK_lse37b9krgqooar93rqq78hh9` (`documento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `persona`
+--
+
+LOCK TABLES `persona` WRITE;
+/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
+/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `apellido` varchar(255) NOT NULL,
+  `clave` varchar(60) NOT NULL,
+  `documento` bigint NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `habilitado` bit(1) DEFAULT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `nombre_usuario` varchar(45) NOT NULL,
+  `tipo_doc` varchar(5) NOT NULL,
+  `id_perfil` int NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `UK_sqdsrgo7yd5nlfxh382v44rj9` (`documento`),
+  UNIQUE KEY `UK_puhr3k3l7bj71hb7hk7ktpxn0` (`nombre_usuario`),
+  KEY `FK131gkl0dt1966rsw6dmesnsxw` (`id_perfil`),
+  CONSTRAINT `FK131gkl0dt1966rsw6dmesnsxw` FOREIGN KEY (`id_perfil`) REFERENCES `perfil` (`id_perfil`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Admin','$2a$10$CS9FgNvM/t9./dd5QKnIYeCT1jV1BD89guGl3zM5kkn2ozGwT88lC',12345678,'admin@admin.com',_binary '','Admin','admin','DNI',1),(2,'Auditor','$2a$10$5KGgRxqjlPtSoAADjmoUcerBO/9HGg04.SL.g4J501Pil76OX/Hp2',98765432,'auditor@auditor.com',_binary '','Auditor','auditor','DNI',2);
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-16 22:41:56
