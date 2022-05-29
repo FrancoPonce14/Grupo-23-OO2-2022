b33c0be8c00fea514fe4ab16bbbc3ecded2128b2
Valen-Topssian <valentintopssian@gmail.com>
2022-05-29 19:14:00 -0300

    repositories 2da parte

---
+++ src/main/java/com/grupo23/Grupo230022022/repositories/IAulaRepository.java
@@ -0,0 +1,13 @@
+package com.grupo23.Grupo230022022.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.grupo23.Grupo230022022.entities.Aula;
+
+
+
+public interface IAulaRepository extends JpaRepository<Aula, Integer> {
+	
+	
+
+}
---
+++ src/main/java/com/grupo23/Grupo230022022/repositories/IEspacioRepository.java
@@ -0,0 +1,13 @@
+package com.grupo23.Grupo230022022.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.grupo23.Grupo230022022.entities.Edificio;
+
+public interface IEspacioRepository extends JpaRepository<Edificio, Integer>{
+	
+	
+
+	
+}
