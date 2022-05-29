7f21e3848e2b59528a4f1955169b7eaff4ba0436
Valen-Topssian <valentintopssian@gmail.com>
2022-05-29 19:38:17 -0300

    actualizacon vista adminIndex

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -93,3 +93,19 @@ update = function() {
 };
 requestAnimationFrame(update);
 
+  $(function(){
+    $('.controler li').click(function(e){
+      $('.controler li').removeClass('active');
+      $(this).addClass('active');
+      var clickVar = $(this).children('a').attr('href');
+      
+      switch (clickVar){
+        case '#one' : $('.rings').attr('data-view','one');
+        break;
+          case '#two' : $('.rings').attr('data-view','two');
+        break;
+          case '#three' : $('.rings').attr('data-view','three');
+        break;
+      }
+    });
+  });
