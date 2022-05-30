e35d90831c967c68396360120ff60eb99bc0e0a1
franco-ponce <francoponce20@hotmail.com>
2022-05-30 01:06:46 -0300

    vistas espacio

--- src/main/java/com/grupo23/Grupo230022022/repositories/IEspacioRepository.java
+++ src/main/java/com/grupo23/Grupo230022022/repositories/IEspacioRepository.java
@@ -4,6 +4,7 @@ package com.grupo23.Grupo230022022.repositories;
 import java.time.LocalDate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.grupo23.Grupo230022022.entities.Aula;
@@ -12,6 +13,7 @@ import com.grupo23.Grupo230022022.entities.Espacio;
 
 public interface IEspacioRepository extends JpaRepository<Espacio, Integer>{
 	
+	@Query(value="select from espacio", nativeQuery=true)
 	public abstract Espacio traer(LocalDate fecha, char turno, @Param("aula") Aula aula); 
 
 	
